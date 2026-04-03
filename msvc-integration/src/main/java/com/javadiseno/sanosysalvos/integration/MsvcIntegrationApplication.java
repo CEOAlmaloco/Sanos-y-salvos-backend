4f91a40f88faab904b0781bb402ea8b67f22fee2
chris <ch.mesa@duocuc.cl>
2026-04-03 20:14:35 -0300

    refactor: reorganiza estructura base de 5 microservicios (ECS)

---
+++ msvc-integration/src/main/java/com/javadiseno/sanosysalvos/integration/MsvcIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.javadiseno.sanosysalvos.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvcIntegrationApplication.class, args);
+	}
+
+}
